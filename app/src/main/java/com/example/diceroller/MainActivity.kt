19694fb2196868373b14f185f00ebd5d015b1141
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-10-09 22:30:21 -0700

    dir "res/layout/" src "activity_main.xml"     Removed TextView and replaced with ImageView (Vectors) dir "java/com.example.diceroller/" src "MainActivity.kt"     - Removed TextView variable "resultText" and import     - Removed Toast Screen display and import     - Replaced the textResult : TextView with     - diceImage : ImageView that holds an #id "dice_image"       that will has the layout of the dice image to beb       displayed.     - Added Kotlin switch statement that chooses the       appropiate dice vector (ImageView) based on the       random number and then set the image resource for       displaying purposes.
          Modulirized into more function with specific
          functionalities:
          - rollDice() : int
          - SetDiceImageViewResource(Int, ImageView) : void

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,25 +2,61 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import java.util.*
+
+const val EMPTY_DICE = 0
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)  // execute layout on activity main
 
         // Create Button obj. Reference id# roll_btn
-        val rollButton: Button = findViewById(R.id.roll_btn);
-        // change text during execution
-        rollButton.text = "Let's Roll!"
+        val rollButton: Button = findViewById(R.id.roll_btn)
 
-        //
-        rollButton.setOnClickListener{
-            Toast.makeText(this,
-                            "button clicked",
-                            Toast.LENGTH_SHORT).show()
-            }
+        // change text during execution
+        rollButton.text = getString(R.string.text_letsRoll)
+
+        // const ImageView that has reference to layout dice_image.
+        // This var attribute will be alter for different displaying pursposes.
+        // The id itself it const
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        // Image src initialize to empty dice
+        setDiceImageViewResource(EMPTY_DICE, diceImage)
+
+        // Event Listener
+        // update the image base on the number return when the dice is roll.
+        rollButton.setOnClickListener {
+            setDiceImageViewResource( rollDice(), diceImage)
+        }
+    }
+
+    // Function that returns an integer number one to size when the dice roll event is executed.
+    private fun rollDice() : Int {
+        // create ramdom int (1- 6)
+        val randomInt : Int = Random().nextInt(6) + 1
+
+        return randomInt
+    }
+
+    // Function that sets the image resource to the acceptable integer values
+    // dicePos: 1- 6 set their respective dice_Pos anyhting elses return empty_dice
+    private fun setDiceImageViewResource(dicePos : Int ,image : ImageView) {
+        // Kotlin Switch statment// Kotlin Switch statment
+        val drawableResource = when(dicePos) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        image.setImageResource(drawableResource)
     }
 }
