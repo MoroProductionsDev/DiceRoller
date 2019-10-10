32b6b207ed4c291c85132b5a4bf96e5a204c4dd2
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-10-09 19:04:52 -0700

    dir [java]     Added Event Listener to react when the roll button has been clicked:     setOnClickListener

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create Button obj. Reference id# roll_btn
+        val rollButton: Button = findViewById(R.id.roll_btn);
+        // change text during execution
+        rollButton.text = "Let's Roll!"
+
+        //
+        rollButton.setOnClickListener{
+            Toast.makeText(this,
+                            "button clicked",
+                            Toast.LENGTH_SHORT).show()
+            }
     }
 }
